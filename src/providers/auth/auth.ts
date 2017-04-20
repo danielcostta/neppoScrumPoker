319731902e2a047de42ab1959dbebe15c8a0dd4e
Carlos Eduardo <dudu.oo@outlook.com>
2017-04-20 18:22:52 -0300

    Adicionado os métodos de login, logout e checar o token.

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -1,12 +1,66 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
+
+import {Storage} from '@ionic/storage';
 
 @Injectable()
 export class AuthProvider {
 
-  constructor(public http: Http) {
-    console.log('Hello AuthProvider Provider');
+  public token: any;
+  public api_url: string;
+
+  constructor(public http: Http, public storage: Storage) {
+    this.api_url = 'http://localhost:8080/';
+  }
+
+  checkAuthentication() {
+    return new Promise((resolve, reject) => {
+
+      //Load token if exists
+      this.storage.get('token').then((value) => {
+
+        this.token = value;
+
+        let headers = new Headers();
+        headers.append('Authorization', this.token);
+
+        this.http.get(this.api_url + 'api/auth/protected', {headers: headers})
+          .subscribe(res => {
+            resolve(res);
+          }, (err) => {
+            reject(err);
+          });
+
+      });
+
+    });
+  }
+
+  login(credentials) {
+    return new Promise((resolve, reject) => {
+
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+
+      this.http.post(this.api_url + 'api/auth/login', JSON.stringify(credentials), {headers: headers})
+        .subscribe(res => {
+
+          let data = res.json();
+          this.token = data.token;
+          this.storage.set('token', data.token);
+          resolve(data);
+
+          resolve(res.json());
+        }, (err) => {
+          reject(err);
+        });
+
+    });
+  }
+
+  logout() {
+    this.storage.set('token', '');
   }
 
 }
