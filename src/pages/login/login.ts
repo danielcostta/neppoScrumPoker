608e32c3c7b7b9aa3aa14aa60a877c08579f2d88
Carlos Eduardo <dudu.oo@outlook.com>
2017-04-20 18:26:16 -0300

    Adicionada função para realizar login e checar token de acesso ao entrar no app

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,25 +1,72 @@
 import {Component} from '@angular/core';
-import {MenuController} from 'ionic-angular';
+import {LoadingController, MenuController, NavController} from 'ionic-angular';
 import {Validators, FormBuilder, FormGroup} from '@angular/forms';
+
+import {AuthProvider} from "../../providers/auth/auth";
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
+  providers: [AuthProvider]
 })
 export class LoginPage {
   private formLogin: FormGroup;
 
-  constructor(public fb: FormBuilder, public menuCtrl: MenuController) {
-    menuCtrl.enable(false);
+  private email: string;
+  private password: string;
+  private loading: any;
+
+  constructor(public fb: FormBuilder,
+              public menuCtrl: MenuController,
+              public authService: AuthProvider,
+              public loadingCtrl: LoadingController,
+              public navCtrl: NavController) {
+    menuCtrl.enable(true);
 
     this.formLogin = fb.group({
-      'username': [null, Validators.required],
+      'email': [null, Validators.required],
       'password': [null, Validators.required]
     })
   }
 
-  submit() {
-    console.log(this.formLogin.value);
+  ionViewDidLoad() {
+    this.showLoader();
+
+    this.authService.checkAuthentication().then((res) => {
+      console.log("Already authorized");
+      this.loading.dismiss();
+      //this.navCtrl.setRoot(HomePage);
+    }, (err) => {
+      console.log("Not already authorized");
+      this.loading.dismiss();
+    });
+  }
+
+  login() {
+    this.showLoader();
+
+    let credentials = {
+      email: this.email,
+      password: this.password
+    };
+
+    this.authService.login(credentials).then((result) => {
+      this.loading.dismiss();
+      console.log(result);
+      //this.navCtrl.setRoot(HomePage);
+    }, (err) => {
+      this.loading.dismiss();
+      console.log(err);
+    });
+
+  }
+
+  showLoader() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Verificando...'
+    });
+
+    this.loading.present();
   }
 
 }
