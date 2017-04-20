30ec2d50188d94da46a990284a4a1c6a241c7663
Carlos Eduardo <dudu.oo@outlook.com>
2017-04-20 18:13:38 -0300

    Importado o IonicStorageModule para poder usar o Storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {ErrorHandler, NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {IonicStorageModule} from '@ionic/storage';
 
 import {MyApp} from './app.component';
 import {LoginPage} from "../pages/login/login";
@@ -20,6 +21,7 @@ import {AuthProvider} from '../providers/auth/auth';
     BrowserModule,
     HttpModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
